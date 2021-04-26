e9ac4b6c8b6099bc74eae95e47bee32cf1e9389b
Troy Dorman <troydorman@gmail.com>
2021-04-25 20:26:26 -0600

    added car picture and created basic app class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import benz from './imgs/benz.jpg'
 
-const App = () => {
-    return (
-        <div>
-            <h1>we are live</h1>
-        </div>
-    )
-}
-
-export default App
+class App extends React.Component {
+    state = {
+      showPaypal: false
+    };
+  
+    showPaypalButtons = () => {
+      this.setState({ showPaypal: true });
+    };
+  
+    render() {
+      const { showPaypal } = this.state;
+      if (showPaypal) {
+        return <PaypalButtons />;
+      } else {
+        return (
+          <div className="main">
+            <h2> Buy this Mercedes at a giveaway price (Super Cheap) </h2>
+            <img alt="Mercedes G-Wagon" src={benz} />
+            <h3>
+              <b>$200</b>
+            </h3>
+            <button onClick={this.showPaypalButtons}> Pay </button>
+          </div>
+        );
+      }
+    }
+  }
+  
+  export default App;
